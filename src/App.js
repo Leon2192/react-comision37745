2d2b053b7c4cab5595ed264d1be1cca728bf265f
Leonardo <leon.strokes@gmail.com>
2022-06-27 16:20:29 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import ResponsiveAppBar from './components/NavBar/NavBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import Home from './Screens/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ResponsiveAppBar />
+        < Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/category/:categoryId' element={<ItemListContainer />} />
+          <Route path='/item/:itemId' element={<ItemDetailContainer />} />
+        </ Routes>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
